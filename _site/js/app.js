49304c2073085e438d272f7a52003d53be18f1da
Chandan Benjaram <cbenjaram@apple.com>
2019-08-09 15:02:48 -0700

    home update

--- _site/js/app.js
+++ _site/js/app.js
@@ -5,16 +5,16 @@ jQuery(document).ready(function($) {
   $(document).foundation();
 
 
-  if($('form#contact_form').length > 0) {
+  if ($('form#contact_form').length > 0) {
     $('form#contact_form').validate({
-      messages: { },
+      messages: {},
       submitHandler: function(form) {
         $.ajax({
           type: 'POST',
           url: 'send.php',
           data: $(form).serialize(),
           success: function(data) {
-            if(data.match(/success/)) {
+            if (data.match(/success/)) {
               $(form).trigger('reset');
               $('#thanks').show().fadeOut(5000);
             }
@@ -34,14 +34,15 @@ jQuery(document).ready(function($) {
     $('#slides').on('init.slides', function() {
       var that = this;
       setTimeout(function() {
-        $('.slides-container', that).children().eq(0).addClass('active');
+        $('.slides-container', that).children().eq(0).addClass(
+          'active');
       }, 300);
     });
 
     $('#slides').superslides({
       animation: 'fade',
       hashchange: false,
-      play: 5000
+      play: 5000000
     });
 
     $('#slides').on('animated.slides', function() {
@@ -51,7 +52,8 @@ jQuery(document).ready(function($) {
       var that = this;
 
       setTimeout(function() {
-        $('.slides-container', that).children().eq(index).addClass('active');
+        $('.slides-container', that).children().eq(index).addClass(
+          'active');
       }, 300);
 
     });
@@ -78,11 +80,11 @@ jQuery(document).ready(function($) {
 
   var bars = $(".bars");
 
-  for(var i = 0; i < bars.length; i++) {
+  for (var i = 0; i < bars.length; i++) {
     var bar = bars[i];
     var highlights = $('> li > .highlighted', $(bar));
 
-    for ( var j = 0; j < highlights.length; j++ ) {
+    for (var j = 0; j < highlights.length; j++) {
 
       var highlight = highlights[j];
 
@@ -92,7 +94,9 @@ jQuery(document).ready(function($) {
         // http://stackoverflow.com/questions/3363035/jquery-animate-forces-style-overflowhidden
         $(this).animate({
           'width': percent + '%'
-        }, 1700, function() { $(highlight).css('overflow', 'visible'); });
+        }, 1700, function() {
+          $(highlight).css('overflow', 'visible');
+        });
       });
 
     }
@@ -117,27 +121,26 @@ jQuery(document).ready(function($) {
       autoplay: true,
       autoplaySpeed: 3000,
       pauseOnHover: false,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
+      responsive: [{
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
         }
-      ]
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }]
     });
   }
 
   $('.back-to-top').on('click', function() {
-    $("html, body").animate({scrollTop: 0}, 1000);
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
     return false;
   });
 
@@ -147,18 +150,25 @@ jQuery(document).ready(function($) {
     var items = $('.gallery li', $ctx);
     var gutter = $ctx.data('gutter');
 
-    if ( !gutter ) { gutter = 0 };
+    if (!gutter) {
+      gutter = 0
+    };
 
-    for( var i = 0; i < items.length; i++ ) {
+    for (var i = 0; i < items.length; i++) {
       var item = items[i];
       $(item).data('masonry-id', i);
     }
 
-    var msnry = new Masonry($('.gallery', $ctx)[0], { itemSelector: 'li', gutter: gutter, isInitLayout: false, columnWidth: ".gallery li:not(.wide)" });
+    var msnry = new Masonry($('.gallery', $ctx)[0], {
+      itemSelector: 'li',
+      gutter: gutter,
+      isInitLayout: false,
+      columnWidth: ".gallery li:not(.wide)"
+    });
 
     window.msnry = msnry;
 
-    $('.gallery', $ctx).imagesLoaded( function() {
+    $('.gallery', $ctx).imagesLoaded(function() {
       msnry.layout();
     });
 
@@ -174,19 +184,21 @@ jQuery(document).ready(function($) {
 
       var cat = $(this).attr('data-cat');
 
-      var gallery = $('.gallery-nav').closest('.masonry-gallery').find('ul.gallery');
+      var gallery = $('.gallery-nav').closest('.masonry-gallery').find(
+        'ul.gallery');
 
       if (cat === 'all') {
         var exists = $('.gallery li', $ctx);
         var elems = [];
 
-        for( var i = 0; i < items.length; i++ ) {
+        for (var i = 0; i < items.length; i++) {
           var item = items[i];
           var skip = false;
 
-          for(var j = 0; j < exists.length; j++ ) {
+          for (var j = 0; j < exists.length; j++) {
             var exist = exists[j];
-            if ($(item).data('masonry-id') == $(exist).data('masonry-id')) {
+            if ($(item).data('masonry-id') == $(exist).data(
+                'masonry-id')) {
               skip = true;
             }
           }
@@ -201,7 +213,7 @@ jQuery(document).ready(function($) {
       } else {
         var lis = $('li', gallery);
 
-        for(var i = 0; i < lis.length; i++) {
+        for (var i = 0; i < lis.length; i++) {
           var li = lis[i];
 
           if (!$(li).hasClass(cat)) {
@@ -212,20 +224,21 @@ jQuery(document).ready(function($) {
         var exists = $('.gallery li', $ctx);
         var elems = [];
 
-        for( var i = 0; i < items.length; i++) {
+        for (var i = 0; i < items.length; i++) {
           var item = items[i];
           var skip = false;
 
-          for( var j = 0; j < exists.length; j++) {
+          for (var j = 0; j < exists.length; j++) {
             var exist = exists[j];
 
-            if ($(item).data('masonry-id') == $(exist).data('masonry-id')) {
+            if ($(item).data('masonry-id') == $(exist).data(
+                'masonry-id')) {
               skip = true;
             }
           }
 
 
-          if ( $(item).hasClass(cat) && !skip) {
+          if ($(item).hasClass(cat) && !skip) {
             ($('.gallery', $ctx)[0]).appendChild($(item)[0]);
             elems.push($(item)[0]);
           }
